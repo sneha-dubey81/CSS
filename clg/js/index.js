3de39ce30639da8462436ab0e8bf7dfffd29ceaa
Sneha Dubey <sneha.dubey2005@gmail.com>
2025-08-25 20:17:18 +0530

    css

---
+++ clg/js/index.js
@@ -0,0 +1,40 @@
+// var a=                  //null
+
+// var a 
+// console.log(a);            //undefined
+
+// var a=5
+// var a=10
+// console.log(a);
+
+// let b=5
+// b=6
+// console.log(b);
+
+// console.log(5==5);               //compare
+// console.log([]==[]);             //false
+// console.log(5==='5');            //checks datatype 
+
+// let a=100
+// let to=a==100
+// console.log(to)
+
+const c=2
+console.log(c);   //cant be changed or reassigned
+
+console.log(s=true)  //assignment
+
+let userName="Sneha"
+let lastName="dubey"
+console.log(`${userName} ${lastName}`)
+
+//ternary operator
+let a=false 
+a?console.log("hiiii"):console.log("byeee");
+
+if(a=true){
+    console.log("hello");
+}
+else{
+    console.log("hiii");
+}
